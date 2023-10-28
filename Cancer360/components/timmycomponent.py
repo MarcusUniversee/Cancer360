757f11cef6e0e3a802580240f8ab087b62d46933
Timothy Gao <timothygao8710@gmail.com>
2023-10-28 16:24:57 -0700

    timmystates working

--- Cancer360/components/timmycomponent.py
+++ Cancer360/components/timmycomponent.py
@@ -1,10 +1,39 @@
 """Sidebar component for the app."""
 
 from Cancer360 import styles
-from Cancer360.timmystates import State, TimmyState
+from Cancer360.timmystates import State, TimmyAppointmentFormState, getPred, text_analysis
+from Cancer360.state import State, FormErrorState, AppointmentFormState
 
 import reflex as rx
 
-def timmy_state() -> rx.Component:
+def timmy_component() -> rx.Component:
     
-    return rx.text("hello world")
+    return rx.vstack(
+        rx.text("Please provide us with a detailed description of your symptom. ", font_size="2em"),
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="Age",
+                    id="Age",
+                ),
+                rx.input(
+                    placeholder="Gender",
+                    id="Gender",
+                ),
+                rx.input(
+                    placeholder="Input",
+                    id="Input",
+                ),
+                rx.button("Submit", type_="submit"),
+            ),
+            on_submit=TimmyAppointmentFormState.handle_submit_data,
+        ),
+        rx.divider(),
+        rx.text(TimmyAppointmentFormState.results.to_string()),
+        
+        width="100%",
+        border_bottom=styles.border,
+        padding="1em",
+    )
+
+#AppointmentFormState.form_data.to_string()
--- Cancer360/timmystates.py
+++ Cancer360/timmystates.py
@@ -2,15 +2,57 @@
 
 import reflex as rx
 import re
+import pickle
+from Cancer360.state import State
+import numpy as np
+from xgboost import XGBClassifier
 
+class TimmyAppointmentFormState(State):
+    form_data: dict = {}
+    data_filled: bool = False
+    results: float
+    def handle_submit_data(self, form_data: dict):
+        self.form_data = form_data
+        self.data_filled = True
+        self.results = text_analysis(form_data)
+        
+    def handle_clear_data(self):
+        self.form_data.clear()
+        self.data_filled = False
 
-class State(rx.State):
-    """Base state for the app.
+# pip freeze > requirements.txt
 
-    The base state is used to store general vars used throughout the app.
-    """
+def text_analysis(data = TimmyAppointmentFormState.form_data):
+    res = []
+    age = int(data["Age"])
+    res.append(age)
+    
+    gender = data["Gender"]
+    main_data = data["Input"] or 'hi :)'
+    
+    if(gender.lower() == "male"):
+        res.append(1)
+        res.append(0)
+    else:
+        res.append(0)
+        res.append(1)
+        
+    relevant_queries = ['ALCOHOL','CHEST PAIN','SHORTNESS OF BREATH','COUGHING','PEER PRESSURE','CHRONIC DISEASE','SWALLOWING DIFFICULTY', 'YELLOW FINGERS', 'ANXIETY', 'FATIGUE', 'ALLERGY', 'WHEEZING']
+    
+    for i in relevant_queries:
+        I = i.lower()
+        if(I in main_data.lower()):
+            res.append(1)
+        else:
+            res.append(0)
 
-    pass
+    return getPred(np.array(res))
+    
+def getPred(x_inputs, path = '../../xgb_model.pkl'):
+    with open("xgb_model.pkl", "rb") as file:
+        loaded_xgb = pickle.load(file)
+        
+    # test = [61, False, True, 1, 1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 2]
+    xgb_pred = loaded_xgb.predict_proba([x_inputs])
 
-class TimmyState(State):
-    pass
+    return float(xgb_pred[0][1] * 100)
